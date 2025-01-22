475ef70b54a1520b3c16d7c28d179b3b532019b8
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-23 01:33:37 +0530

    Middleware to parse JSON bodies

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ require('dotenv').config(); // Load environment variables
 
 const express = require('express');
 
+const cors = require('cors'); // Import CORS
+
 const app = express();
 const host = process.env.HOST || '0.0.0.0'; // Changed to 'localhost'
 const port = process.env.PORT || 5000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(cors()); // Enable CORS for all routes
 
 app.listen(port, () => {
     console.log(`Server running on http://${host}:${port}`);
